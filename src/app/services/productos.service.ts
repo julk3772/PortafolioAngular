c278ef3428be209a55f776e42d39f83338b28025
julk3390 <juan3390@gmail.com>
2020-05-08 20:30:40 -0400

    se colocaron los archivos de productos, no carga bien

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -1,18 +1,21 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Producto } from '../interfaces/producto.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductosService {
   cargando = true;
+  productos: Producto[] = [] ;
   constructor( private http: HttpClient) {
     this.cargarProductos();
    }
    private cargarProductos() {
      this.http.get('https://angular-html-72e05.firebaseio.com/productos_idx.json')
-      .subscribe( (resp: any[]) => {
+      .subscribe( (resp: Producto[]) => {
           console.log(resp);
+          this.productos = resp;
           this.cargando = false;
       });
    }

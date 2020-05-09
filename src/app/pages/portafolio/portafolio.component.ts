008f862dcfb90eac129a3cc75a980ec4ac666219
julk3390 <juan3390@gmail.com>
2020-05-09 19:07:05 -0400

    se pudo corregir el error de portafolio era productos

--- src/app/pages/portafolio/portafolio.component.ts
+++ src/app/pages/portafolio/portafolio.component.ts
@@ -8,7 +8,7 @@ import { ProductosService } from 'src/app/services/productos.service';
 })
 export class PortafolioComponent implements OnInit {
 
-  constructor( public productoService: ProductosService) { }
+  constructor( public productosService: ProductosService) { }
 
   ngOnInit(): void {
   }

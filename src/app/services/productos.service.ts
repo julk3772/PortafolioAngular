57d0822e8f620c83ee087bf86131534ff548af56
julk3390 <juan3390@gmail.com>
2020-05-17 21:13:11 -0400

    se termino el filtrado de busqueda

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -13,19 +13,36 @@ export class ProductosService {
     this.cargarProductos();
   }
   private cargarProductos() {
-    this.http.get('https://angular-html-72e05.firebaseio.com/productos_idx.json')
+    return  new Promise ( ( resolve, reject ) => {
+      this.http.get('https://angular-html-72e05.firebaseio.com/productos_idx.json')
       .subscribe( (resp: Producto[]) => {
           
           this.productos = resp;
           this.cargando = false;
       });
+    });    
   }
   getProducto( id: string ) {
     return this.http.get(`https://angular-html-72e05.firebaseio.com/productos/${ id }.json`)
   }
   buscarProducto( termino: string ) {
-    this.productosFiltrado = this.productos.filter( producto => {
-      return true;
-    })
+    if (this.productos.length == 0) {
+      this.cargarProductos().then( () => {
+        this.filtrarProductos( termino );
+      });
+    }else {
+
+    }
+    
+  }
+  private filtrarProductos( termino: string) {
+    this.productosFiltrado = [];
+    termino = termino.toLocaleLowerCase();
+    this.productos.forEach( prod => {
+      const tituloLower = prod.titulo.toLocaleLowerCase();
+      if( prod.categoria.indexOf( termino ) >= 0 || tituloLower.indexOf( termino ) >= 0){
+        this.productosFiltrado.push( prod );
+      }
+    });
   }
 }
